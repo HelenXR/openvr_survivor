dee730890546c37183e97ff0bc27a4e6e998936e
HelenXR <helenhololens@gmail.com>
2017-05-04 14:03:44 +0800

    add glog module as a log system.

--- src/common.h
+++ src/common.h
@@ -1,6 +1,18 @@
 #ifndef OPENVR_SURVIVOR_SRC_COMMON_H_
 #define OPENVR_SURVIVOR_SRC_COMMON_H_
 #include <openvr_driver.h>
+#include <iostream>
+#include <windows.h>
+//In order to prevent ERROR and GLOG / windows.h in the header file ERROR to redefine the problem. 
+//This macro must be defined in the logging.h contains before
+#define GLOG_NO_ABBREVIATED_SEVERITIES 
+#include <glog/logging.h>
 
+//namespace
+using namespace std;
+using namespace vr;
+
+//import lib
+#pragma comment(lib,"libglog.lib")
 
 #endif
--- src/openvr_survivor.cc
+++ src/openvr_survivor.cc
@@ -1,9 +1,18 @@
 #include "common.h"
+
 #define HMD_DLL_EXPORT extern "C" __declspec( dllexport )
 
-using namespace vr;
 HMD_DLL_EXPORT void *HmdDriverFactory(const char *pInterfaceName, int *pReturnCode)
 {
+	//set log dir:steam/logs,you can set,you can set the path you want, but you need to make sure 
+	//that the folder exists.
+	FLAGS_log_dir = "../../../logs";
+    //programe name:"openvr_survivor_glog"
+    google::InitGoogleLogging("openvr_survivor_glog");
+    LOG(INFO) << "google log first info level message!";
+    //...... DoSomething
+    //Shutdown google's logging library.
+    google::ShutdownGoogleLogging();
 	if (0 == strcmp(IServerTrackedDeviceProvider_Version, pInterfaceName))
 	{
 		//return &g_serverDriverNull;
